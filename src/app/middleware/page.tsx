05f9d55ac54e558437cffc09d55eb56777740c3c
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-05 17:10:18 +0700

    Protecting routes with middleware

---
+++ src/app/middleware/page.tsx
@@ -0,0 +1,11 @@
+import { auth } from '@/auth'
+
+export default async function MiddlewarePage() {
+	const session = await auth()
+	return (
+		<main className='flex h-full items-center justify-center flex-col gap-2'>
+			<h1 className='text-3xl'>Middleware Page</h1>
+			<p className='text-lg'>{session?.user?.email}</p>
+		</main>
+	)
+}
