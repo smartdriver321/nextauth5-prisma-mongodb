232ee57bfc87dc1c6bc1ce636cdf41ad1be12ced
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-09-05 17:17:42 +0700

    Protecting APIs

---
+++ src/app/api/data/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server'
+
+import { auth } from '@/auth'
+
+export const GET = auth(function GET(req) {
+	if (req.auth) return NextResponse.json(req.auth)
+
+	return NextResponse.json({ message: 'Not Authenticated' }, { status: 401 })
+})
